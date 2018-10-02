5c97b85b0f0cf952fbda178828a1ecc962a5ee37
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-02 13:12:58 +0100

    Created quotient function
    Added quotient function that takes in 2 doubles and returns a double. Added statement to execute quotient function and output to console.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,6 +10,10 @@ void Product(int a, int b) {
 	cout << "Product of a and b: " << a * b;
 }
 
+double Quotient(double a, double b) {
+	return a / b;
+}
+
 int main() {
 	cout << "Hello" << ' ' << "World" << "\n";
 	std::cout << "Type 2 numbers, i'll calculate and display the sum and product." << "\n";
@@ -17,6 +21,7 @@ int main() {
 	cin >> a;
 	cin >> b;
 	Product(a, b);
+	cout << "\nQuotient of a and b: " << Quotient(a, b);
 	cin >> b;
 
 	return 0;
